20e1ad12e8cfb43aee02db20817a14393b4f0900
Muhammad-Hashir-55 <kingrocky742@gmail.com>
2024-06-03 17:13:47 +0500

    final new line or added the last line to simpleprogram.cpp

--- simpleprogram.cpp
+++ simpleprogram.cpp
@@ -3,5 +3,6 @@ using namespace std; int main (){
  cout <<"hello world"<<endl;
 cout <<"this program was written using nano command in git "<<endl;
 cout <<"new line"<<endl;
+cout <<"last new line"<<endl;
 return 0;
 }
